d448419346c93b8296bee35705b8ccf091629910
Nika <129855721+Nika-cyber1@users.noreply.github.com>
2023-09-08 12:34:32 +0300

    Update tags.js

--- tags.js
+++ tags.js
@@ -1,16 +1,16 @@
 module.exports = async (waw) => {
 	waw.tags = async (query = {}) => {
-		return waw.cache("tags" + JSON.stringify(query), async () => {
-			return await waw.Tag.find(query).sort('category');
-		});
+	//	return waw.cache("tags" + JSON.stringify(query), async () => {	
+			return await waw.Tag.find(query).sort('category');	
+	//	});
 	};
 	waw.tagsWithCategories = async (query = {}) => {
-		return waw.cache("tags" + JSON.stringify(query), async () => {
+	//	return waw.cache("tags" + JSON.stringify(query), async () => {
 			return await waw.Tag.find(query).sort('category').populate({
 				path: 'category',
 				select: 'name group'
 			});
-	 });
+//	 });
 	};
 
 	waw.crud('tag', {
