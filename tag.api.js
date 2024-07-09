44f9db3274617caa39b065e7d028b113b28b88aa
Narolskyi Denis <asotizianov@gmail.com>
2024-07-09 19:45:23 +0300

    latest ecommerce updates

--- tag.api.js
+++ tag.api.js
@@ -1,69 +1,74 @@
 module.exports = async (waw) => {
+	const ensure = waw.role("admin owner", async (req, res, next) => {
+		if (!req.user.is.admin) {
+			req.storeIds = (
+				await waw.Store.find({
+					moderators: req.user._id,
+				}).select("_id")
+			).map((s) => s.id);
+		}
+		next();
+	});
 	waw.crud("tag", {
 		create: {
 			ensure: waw.role("admin owner"),
 		},
 		unique: {
-			name: 'url',
-			ensure: waw.role("admin owner"),
+			name: "url",
+			ensure,
 			query: (req) => {
 				return req.user.is.admin
-					? {}
+					? {
+						_id: req.body._id,
+					}
 					: {
-						$or: [
-							{
-								global: true,
-								_id: req.body._id
-							},
-							{
-								author: req.user._id,
-								_id: req.body._id
-							},
-						],
+						_id: req.body._id,
+						stores: {
+							$in: req.storeIds,
+						},
 					};
-			}
+			},
 		},
 		get: {
-			ensure: waw.role("admin owner"),
+			ensure,
 			query: (req) => {
 				return req.user.is.admin
 					? {}
 					: {
-							$or: [
-								{
-									global: true,
-								},
-								{
-									author: req.user._id,
-								},
-							],
-					  };
+						stores: {
+							$in: req.storeIds,
+						},
+					};
 			},
 		},
 		update: {
-			ensure: waw.role("admin owner"),
+			ensure,
 			query: (req) => {
 				return req.user.is.admin
 					? {
-							_id: req.body._id,
-					  }
+						_id: req.body._id,
+					}
 					: {
-							_id: req.body._id,
-							author: req.user._id,
-					  };
+						_id: req.body._id,
+						stores: {
+							$in: req.storeIds,
+						},
+					};
 			},
 		},
 		delete: {
-			ensure: waw.role("admin owner"),
+			ensure,
 			query: (req) => {
 				return req.user.is.admin
 					? {
-							_id: req.body._id,
-					  }
+						_id: req.body._id,
+					}
 					: {
-							_id: req.body._id,
-							author: req.user._id,
-					  };
+						_id: req.body._id,
+						stores: {
+							$in: req.storeIds,
+						},
+					};
 			},
 		},
 	});
@@ -100,12 +105,24 @@ module.exports = async (waw) => {
 		while (tag.parent) {
 			tag = await waw.Tag.findById(tag.parent);
 			tag.children = tag.children || [];
-			tag.children = tag.children.filter((t) => !ids.includes(t.toString()));
+			tag.children = tag.children.filter(
+				(t) => !ids.includes(t.toString())
+			);
 			await tag.save();
 		}
 	});
 
 	const reloads = {};
+	waw.addJson(
+		"storeClearTags",
+		async (store, fillJson, req) => {
+			for (const tag of fillJson.allTags) {
+				tag.tags = [];
+				tag.active = false;
+			}
+		},
+		"CLear updatable documents of tags"
+	);
 	waw.addJson(
 		"storePrepareTags",
 		async (store, fillJson, req) => {
@@ -113,20 +130,27 @@ module.exports = async (waw) => {
 			const fillAllTags = async () => {
 				fillJson.allTags = await waw.Tag.find({
 					stores: store.id,
-					enabled: true
-				}).sort({
-					order: 1
-				}).lean();
+					enabled: true,
+				})
+					.sort({
+						order: 1,
+					})
+					.lean();
 
 				for (const tag of fillJson.allTags) {
-					tag.children = (tag.children || []).map(id => id.toString());
-					tag.parent = tag.parent && tag.parent.toString() || '';
+					tag.children = (tag.children || []).map((id) =>
+						id.toString()
+					);
+					tag.parent = (tag.parent && tag.parent.toString()) || "";
 					tag._id = tag._id.toString();
 					tag.id = tag._id.toString();
+
+					tag.allTags = fillJson.allTags.filter(t => t.parent == tag._id);
 				}
 				fillJson.tags = fillJson.allTags.filter((t) => {
 					return !t.parent;
 				});
+
 				fillJson.tagsIds = fillJson.allTags.reduce(
 					(accumulator, currentDocument) => {
 						return accumulator
@@ -135,6 +159,7 @@ module.exports = async (waw) => {
 					},
 					[]
 				);
+
 				// fillJson.tagsIds = []; // temp lock
 			};
 			fillAllTags();
@@ -143,8 +168,8 @@ module.exports = async (waw) => {
 		"Prepare updatable documents of tags"
 	);
 	const tagsUpdate = async (tag) => {
-		for (const storeId of (tag.stores || [])) {
-			for (const reload of (reloads[storeId] || [])) {
+		for (const storeId of tag.stores || []) {
+			for (const reload of reloads[storeId] || []) {
 				reload();
 			}
 		}
